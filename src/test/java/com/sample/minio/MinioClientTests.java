90651ad06ba14832c0e2722fde9776d45fb41a18
qlhan <qlhan@incarcloud.com>
2022-08-18 16:53:52 +0800

    0.1.0-SNAPSHOT Initial commit

---
+++ src/test/java/com/sample/minio/MinioClientTests.java
@@ -0,0 +1,19 @@
+package com.sample.minio;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * MinioClientTests
+ *
+ * @author Aaric, created on 2022-08-18T16:50.
+ * @version 0.1.0-SNAPSHOT
+ */
+@Slf4j
+public class MinioClientTests {
+
+    @Test
+    public void testHello() {
+        log.info("hello minio");
+    }
+}
