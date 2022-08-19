94bcd150bdcacc35bf8c7abf299ff07ea3b3982e
qlhan <qlhan@incarcloud.com>
2022-08-19 10:42:19 +0800

    0.2.0-SNAPSHOT java client

--- src/test/java/com/sample/minio/MinioClientTests.java
+++ src/test/java/com/sample/minio/MinioClientTests.java
@@ -1,6 +1,10 @@
 package com.sample.minio;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,8 +16,24 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class MinioClientTests {
 
+    public static final String MINIO_ENDPOINT = "http://10.0.11.25:9000";
+    public static final String MINIO_ACCESS_KEY = "newuser";
+    public static final String MINIO_SECRET_KEY = "newuser123";
+    public static final String MINIO_BUCKET = "newbucket";
+
+    public static MinioClient client;
+
+    @BeforeAll
+    public static void setUp() {
+        client = MinioClient.builder()
+                .endpoint(MINIO_ENDPOINT)
+                .credentials(MINIO_ACCESS_KEY, MINIO_SECRET_KEY)
+                .build();
+    }
+
     @Test
-    public void testHello() {
-        log.info("hello minio");
+    public void testBucketExists() throws Exception {
+        BucketExistsArgs args = BucketExistsArgs.builder().bucket(MINIO_BUCKET).build();
+        Assertions.assertTrue(client.bucketExists(args));
     }
 }
