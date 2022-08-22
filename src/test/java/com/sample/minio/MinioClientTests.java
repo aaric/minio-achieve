ac039de6a4d3a1ea9ed6f64d666c733ad30afdfa
qlhan <qlhan@incarcloud.com>
2022-08-22 14:06:27 +0800

    0.2.0-SNAPSHOT java client

--- src/test/java/com/sample/minio/MinioClientTests.java
+++ src/test/java/com/sample/minio/MinioClientTests.java
@@ -19,7 +19,7 @@ import java.util.List;
  * MinioClientTests
  *
  * @author Aaric, created on 2022-08-18T16:50.
- * @version 0.1.0-SNAPSHOT
+ * @version 0.2.0-SNAPSHOT
  */
 @Slf4j
 public class MinioClientTests {
